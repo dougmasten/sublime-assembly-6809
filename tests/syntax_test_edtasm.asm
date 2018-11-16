cfe869ad0429106f9b66fe4c8e83410627291b7a
Doug Masten <doug@dougmasten.com>
2018-11-16 05:31:49 -0600

    Add Edtasm file compatible

---
+++ tests/syntax_test_edtasm.asm
@@ -0,0 +1,18 @@
+# SYNTAX TEST "Packages/Assembly-6809/Assembly-6809.sublime-syntax"
+# <- source.asm.6809
+
+; Edtasm assembly file
+
+
+01260 NOZERO  LEAU   4,U
+# <- comment.line
+#^^^^ comment.line
+#    ^ - comment.line
+#     ^^^^^^ entity.name.type.constant
+#           ^^ - comment.line
+#             ^^^^ keyword.operator
+#                 ^^^ - comment.line
+#                    ^ constant.numeric.decimal
+#                     ^ operator.separator
+#                      ^ constant.language.register
+
