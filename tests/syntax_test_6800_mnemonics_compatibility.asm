1fd8511f7b0d5a8a68a4f64e01e911d510aebb23
Doug Masten <doug@dougmasten.com>
2018-12-20 06:47:35 -0600

    add 6800 source code compatibility

---
+++ tests/syntax_test_6800_mnemonics_compatibility.asm
@@ -0,0 +1,88 @@
+# SYNTAX TEST "Packages/Assembly-6809/Assembly-6809.sublime-syntax"
+# <- source.asm.6809
+
+        opt 6800compat
+
+
+; Motorola 6800 mnemonics supported for compatibility
+
+        aba
+#       ^^^ keyword.operator
+
+        cba
+#       ^^^ keyword.operator
+
+        clc
+#       ^^^ keyword.operator
+
+        clf
+#       ^^^ keyword.operator
+
+        cli
+#       ^^^ keyword.operator
+
+        clif
+#       ^^^^ keyword.operator
+
+        clv
+#       ^^^ keyword.operator
+
+        cpx #0
+#       ^^^ keyword.operator
+
+        des
+#       ^^^ keyword.operator
+
+        dex
+#       ^^^ keyword.operator
+
+        dey
+#       ^^^ keyword.operator
+
+        ins
+#       ^^^ keyword.operator
+
+        inx
+#       ^^^ keyword.operator
+
+        iny
+#       ^^^ keyword.operator
+
+        sba
+#       ^^^ keyword.operator
+
+        sec
+#       ^^^ keyword.operator
+
+        sef
+#       ^^^ keyword.operator
+
+        sei
+#       ^^^ keyword.operator
+
+        seif
+#       ^^^^ keyword.operator
+
+        sev
+#       ^^^ keyword.operator
+
+        tab
+#       ^^^ keyword.operator
+
+        tap
+#       ^^^ keyword.operator
+
+        tba
+#       ^^^ keyword.operator
+
+        tpa
+#       ^^^ keyword.operator
+
+        tsx
+#       ^^^ keyword.operator
+
+        txs
+#       ^^^ keyword.operator
+
+        wai
+#       ^^^ keyword.operator
