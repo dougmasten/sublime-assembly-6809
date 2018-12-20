de96c646837d45f37d0636ce9bb92e70768d35e1
Doug Masten <doug@dougmasten.com>
2018-12-20 06:34:22 -0600

    refactor pragmas

--- tests/syntax_test_pragmas.asm
+++ tests/syntax_test_pragmas.asm
@@ -4,29 +4,22 @@
 ; Assembly pragmas (lwasm)
 
 
-  pragma 6800compat
+;------------------------------------------------
+  pragma dollarlocal
 # ^^^^^^ support.function.directive.assembler
-#        ^^^^^^^^^^ keyword.operator
+#        ^^^^^^^^^^^ keyword.operator
 
-  pragma 6809
+  pragma dollarnotlocal
 # ^^^^^^ support.function.directive.assembler
-#        ^^^^ keyword.operator
-
-  pragma 6309
-# ^^^^^^ support.function.directive.assembler
-#        ^^^^ keyword.operator
-
-  pragma 6809conv
-# ^^^^^^ support.function.directive.assembler
-#        ^^^^^^^^ keyword.operator
-
-  pragma 6309conv
-# ^^^^^^ support.function.directive.assembler
-#        ^^^^^^^^ keyword.operator
+#        ^^^^^^^^^^^^^^ keyword.operator
 
   pragma index0tonone
 # ^^^^^^ support.function.directive.assembler
 #        ^^^^^^^^^^^^ keyword.operator
+
+  pragma undefextern
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^ keyword.operator
 
   pragma cescapes
 # ^^^^^^ support.function.directive.assembler
@@ -36,22 +29,6 @@
 # ^^^^^^ support.function.directive.assembler
 #        ^^^^^^^^^^^^^^^^^ keyword.operator
 
-  pragma undefextern
-# ^^^^^^ support.function.directive.assembler
-#        ^^^^^^^^^^^ keyword.operator
-
-  pragma export
-# ^^^^^^ support.function.directive.assembler
-#        ^^^^^^ keyword.operator
-
-  pragma dollarlocal
-# ^^^^^^ support.function.directive.assembler
-#        ^^^^^^^^^^^ keyword.operator
-
-  pragma dollarnotlocal
-# ^^^^^^ support.function.directive.assembler
-#        ^^^^^^^^^^^^^^ keyword.operator
-
   pragma pcaspcr
 # ^^^^^^ support.function.directive.assembler
 #        ^^^^^^^ keyword.operator
@@ -60,9 +37,17 @@
 # ^^^^^^ support.function.directive.assembler
 #        ^^^^^^ keyword.operator
 
+  pragma list
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^ keyword.operator
+
   pragma autobranchlength
 # ^^^^^^ support.function.directive.assembler
 #        ^^^^^^^^^^^^^^^^ keyword.operator
+
+  pragma export
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^ keyword.operator
 
   pragma nosymbolcase
 # ^^^^^^ support.function.directive.assembler
@@ -76,17 +61,21 @@
 # ^^^^^^ support.function.directive.assembler
 #        ^^^^^^^^^^^^^ keyword.operator
 
-  pragma forwardrefmax
-# ^^^^^^ support.function.directive.assembler
-#        ^^^^^^^^^^^^^ keyword.operator
-
-  pragma qrts
+  pragma 6809
 # ^^^^^^ support.function.directive.assembler
 #        ^^^^ keyword.operator
 
-  pragma m80ext
+  pragma 6309
 # ^^^^^^ support.function.directive.assembler
-#        ^^^^^^ keyword.operator
+#        ^^^^ keyword.operator
+
+  pragma 6800compat
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^ keyword.operator
+
+  pragma forwardrefmax
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^^ keyword.operator
 
   pragma testmode
 # ^^^^^^ support.function.directive.assembler
@@ -96,6 +85,10 @@
 # ^^^^^^ support.function.directive.assembler
 #        ^ keyword.operator
 
+  pragma cc
+# ^^^^^^ support.function.directive.assembler
+#        ^^ keyword.operator
+
   pragma cd
 # ^^^^^^ support.function.directive.assembler
 #        ^^ keyword.operator
@@ -104,10 +97,173 @@
 # ^^^^^^ support.function.directive.assembler
 #        ^^ keyword.operator
 
-  pragma cc
+  pragma qrts
 # ^^^^^^ support.function.directive.assembler
-#        ^^ keyword.operator
+#        ^^^^ keyword.operator
 
+  pragma m80ext
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^ keyword.operator
+
+  pragma 6809conv
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^ keyword.operator
+
+  pragma 6309conv
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^ keyword.operator
+
+  pragma newsource
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^ keyword.operator
+
+  pragma oldsource
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^ keyword.operator
+
+  pragma operandsizewarning
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^^^^^^^ keyword.operator
+
+  pragma emuext
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^ keyword.operator
+
+  pragma output
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^ keyword.operator
+
+  pragma expandcond
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^ keyword.operator
+
+;------------------------------------------------
+  pragma nodollarlocal
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^^ keyword.operator
+
+  pragma nodollarnotlocal
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^^^^^ keyword.operator
+
+  pragma noindex0tonone
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^^^ keyword.operator
+
+  pragma noundefextern
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^^ keyword.operator
+
+  pragma nocescapes
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^ keyword.operator
+
+  pragma noimportundefexport
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^^^^^^^^ keyword.operator
+
+  pragma nopcaspcr
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^ keyword.operator
+
+  pragma noshadow
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^ keyword.operator
+
+  pragma nolist
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^ keyword.operator
+
+  pragma noautobranchlength
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^^^^^^^ keyword.operator
+
+  pragma noexport
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^ keyword.operator
+
+  pragma nosymbolcase
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^ keyword.operator
+
+  pragma nosymbolnocase
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^^^ keyword.operator
+
+  pragma nocondundefzero
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^^^^ keyword.operator
+
+  pragma no6800compat
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^ keyword.operator
+
+  pragma noforwardrefmax
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^^^^ keyword.operator
+
+  pragma notestmode
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^ keyword.operator
+
+  pragma noc
+# ^^^^^^ support.function.directive.assembler
+#        ^^^ keyword.operator
+
+  pragma nocc
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^ keyword.operator
+
+  pragma nocd
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^ keyword.operator
+
+  pragma noct
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^ keyword.operator
+
+  pragma noqrts
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^ keyword.operator
+
+  pragma nom80ext
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^ keyword.operator
+
+  pragma no6809conv
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^ keyword.operator
+
+  pragma no6309conv
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^ keyword.operator
+
+  pragma nonewsource
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^ keyword.operator
+
+  pragma nooldsource
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^ keyword.operator
+
+  pragma nooperandsizewarning
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^^^^^^^^^ keyword.operator
+
+  pragma noemuext
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^ keyword.operator
+
+  pragma nooutput
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^ keyword.operator
+
+  pragma noexpandcond
+# ^^^^^^ support.function.directive.assembler
+#        ^^^^^^^^^^^^ keyword.operator
+
+
+;------------------------------------------------
   *pragma c
 # ^^^^^^^ support.function.directive.assembler
 #         ^ keyword.operator
@@ -120,6 +276,8 @@
 # ^^^^^^^^^^ support.function.directive.assembler
 #            ^ keyword.operator
 
+
+;------------------------------------------------
   opt 6800compat
 # ^^^ support.function.directive.assembler
 #     ^^^^^^^^^^ keyword.operator
@@ -234,14 +392,3 @@
 #             ^ operator.separator
 #              ^^^^^^^^^^ keyword.operator
 
-  pragma nolist
-# ^^^^^^ support.function.directive.assembler
-#        ^^^^^^ keyword.operator
-
-  pragma notestmode
-# ^^^^^^ support.function.directive.assembler
-#        ^^^^^^^^^^ keyword.operator
-
-  opt nolist
-# ^^^ support.function.directive.assembler
-#     ^^^^^^ keyword.operator
