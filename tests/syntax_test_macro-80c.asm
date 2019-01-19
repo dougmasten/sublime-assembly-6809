a389a21d9ef1020b5d3f8df344ea806442351732
Doug Masten <doug@dougmasten.com>
2019-01-19 16:17:51 -0600

    add macro-80c assembler's "?rts" branch target

---
+++ tests/syntax_test_macro-80c.asm
@@ -0,0 +1,8 @@
+# SYNTAX TEST "Packages/Assembly-6809/Assembly-6809.sublime-syntax"
+# <- source.mc6809
+
+; MACRO-80c assembler add-ons
+
+;--------------------------------------------------------------------
+        ?rts
+#       ^^^^ keyword.operator
