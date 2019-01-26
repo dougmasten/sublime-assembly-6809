9d2f6183b76e918a2d96bb135143230c37b1b1de
Doug Masten <doug@dougmasten.com>
2019-01-26 03:57:18 -0600

    Add preprocessor add-ons "REPEAT xx" and "ENDREPEAT" directives

---
+++ tests/syntax_test_addon_directives.asm
@@ -0,0 +1,13 @@
+# SYNTAX TEST "Packages/Assembly-6809/Assembly-6809.sublime-syntax"
+# <- source.mc6809
+
+; Preprocessor addon directives
+
+
+ repeat 2
+#^^^^^^ support.function.directive.addon
+#       ^ constant.numeric.decimal
+
+ endrepeat
+#^^^^^^^^^ support.function.directive.addon
+
